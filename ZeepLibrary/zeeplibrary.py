6ac096fba8b1a8276ce385e46d929d6d4c9741b9
Bart Kleijngeld <bart.kleijngeld@alliander.com>
2018-10-25 13:05:05 +0200

    added a lot of regression tests; added some keywords regarding closing and logging

--- ZeepLibrary/zeeplibrary.py
+++ ZeepLibrary/zeeplibrary.py
@@ -5,7 +5,33 @@ import requests.auth
 from robot.api import logger
 from robot.api.deco import keyword
 from lxml import etree
-from .exceptions import *
+
+
+class ZeepLibraryException(Exception):
+    def __str__(self):
+        return self.message
+
+
+class AliasAlreadyInUseException(ZeepLibraryException):
+    def __init__(self, alias):
+        self.message = "The alias `{}' is already in use.".format(alias)
+
+
+class ClientNotFoundException(ZeepLibraryException):
+    def __init__(self, alias):
+        self.message = "Could not find a client with alias `{}'."\
+                           .format(alias)
+
+class AliasNotFoundException(ZeepLibraryException):
+    def __init__(self):
+        self.message = "Could not find alias for the provided client."
+
+
+class AliasRequiredException(ZeepLibraryException):
+    def __init__(self):
+        self.message = ("When using more than one client, providing an alias "
+                        "is required.")
+
 
 class ZeepLibrary:
     """This library is built on top of the library Zeep in order to bring its
@@ -31,36 +57,37 @@ class ZeepLibrary:
     def active_client_alias(self):
         return self._active_client_alias
 
+    @active_client_alias.setter
+    def active_client_alias(self, alias):
+        if alias not in self._clients.keys():
+            raise ClientNotFoundException(alias)
+
+        self._active_client_alias = alias
+    
     @property
     def clients(self):
         return self._clients
 
-    @property
-    def client_count(self):
-        return len(self._clients)
+    @keyword('Close client')
+    def close_client(self, alias=None):
+        """Closes an opened Zeep client.
 
-    @active_client_alias.setter
-    def active_client_alias(self, alias):
-        if alias not in self._clients.keys():
-            raise AliasNotFoundException(alias)
+        If no ``alias`` is provided, the active client will be assumed.
+        """
+        if not alias:
+            alias = self.active_client_alias
+        self.clients.pop(alias, None)
 
-        self._active_client_alias = alias
+    @keyword('Close all clients')
+    def close_all_clients(self):
+        for alias in self.clients.keys():
+            self.close_client(alias)
 
-    @property
-    def namespace_prefix_map(self):
-        return { prefix: namespace
-                 for prefix, namespace in self.active_client
-                     .wsdl
-                     .types
-                     .prefix_map
-                     .items() }
-
-    @keyword('Switch client')
-    def switch_client(self, alias):
-        current_active_client_alias = self.active_client_alias
+    def _add_client(self, client, alias=None):
+        if alias is None and len(self.clients) > 0:
+            raise AliasRequiredException
+        self.clients[alias] = client
         self.active_client_alias = alias
-
-        return current_active_client_alias
 
     @keyword('Create client')
     def create_client(self,
@@ -79,37 +106,83 @@ class ZeepLibrary:
         transport = zeep.transports.Transport(session=session)
 
         client = zeep.Client(wsdl, transport=transport)
-        self.add_client(client, alias)
+        self._add_client(client, alias)
 
-    def add_client(self, client, alias=None):
-        if alias is not None and self.client_count > 0:
-            raise AliasRequiredException()
-        self.clients[alias] = client
-        self.active_client_alias = alias
+        return client
+
+    @keyword('Create message')
+    def create_message(self, operation, to_string=False, **kwargs):
+        message = self.active_client.create_message(\
+            self.active_client.service,
+            operation,
+            **kwargs)
+        if to_string:
+            return etree.tostring(message)
+        else:
+            return message
 
     @keyword('Create object')
     def create_object(self, type, *args, **kwargs):
         type_ = self.active_client.get_type(type)
         return type_(*args, **kwargs)
 
-    @keyword('Get namespace URI')
-    def get_namespace_uri_by_prefix(self, prefix):
-        return self.namespace_prefix_map[prefix]
+    @keyword('Get alias')
+    def get_alias(self, client=None):
+        if not client:
+            return self.active_client_alias
+        else:
+            for alias, client_ in self.clients.iteritems():
+                if client_ == client:  return alias
+        raise AliasNotFoundException()
+
+    @keyword('Get client')
+    def get_client(self, alias=None):
+        """Gets the ``Zeep.Client`` object.
+
+        If no ``alias`` is provided, the active client will be assumed.
+        """
+        if alias:
+            return self.clients[alias]
+        else:
+            return self.active_client
+
+    @keyword('Get clients')
+    def get_clients(self):
+        return self.clients
 
     @keyword('Get namespace prefix')
     def get_namespace_prefix_for_uri(self, uri):
-        for prefix, uri_ in self.namespace_prefix_map.iteritems():
+        for prefix, uri_ in self.active_client.namespaces.iteritems():
             if uri == uri_:
                 return prefix
 
-    @keyword('Create message as XML node')
-    def create_message_as_xml(self, operation, **kwargs):
-        return self.active_client.create_message(self.active_client.service,
-                                                 operation,
-                                                 **kwargs)
+    @keyword('Get namespace URI')
+    def get_namespace_uri_by_prefix(self, prefix):
+        return self.active_client.namespaces[prefix]
 
-    @keyword('Create message string')
-    def create_message_as_string(self, operation, **kwargs):
-        msg = self.create_message_as_xml(operation, **kwargs)
-        return etree.tostring(msg)
+    @keyword('Log namespace prefix map')
+    def log_namespace_prefix_map(self, to_log=True, to_console=False):
+        _log(self.active_client.namespaces, to_log, to_console)
 
+    @keyword('Log opened clients')
+    def log_opened_clients(self, to_log=True, to_console=False):
+        _log(self.clients, to_log, to_console)
+
+    @keyword('Log WSDL dump')
+    def dump_wsdl(self):
+        self.active_client.wsdl.dump()
+
+    @keyword('Switch client')
+    def switch_client(self, alias):
+        current_active_client_alias = self.active_client_alias
+        self.active_client_alias = alias
+
+        return current_active_client_alias
+
+
+# Utility functions.
+def _log(item, to_log=True, to_console=False):
+    if to_log:
+        logger.info(item, also_console=to_console)
+    elif to_console:
+        logger.console(item)
--- tests/regression_set.robot
+++ tests/regression_set.robot
@@ -1,5 +1,144 @@
 *** Settings ***
 Library  ZeepLibrary
+Library  Collections
+Library  XML  use_lxml=${TRUE}
+
+
+
+*** Variables ***
+${CALCULATOR WSDL}  ${CURDIR}${/}..${/}calculator.wsdl
+${BLZSERVICE WSDL}  ${CURDIR}${/}..${/}blzservice.wsdl
+
 
 
 *** Test Cases ***
+Create client and close
+    Create client  ${CALCULATOR WSDL}
+    ${clients}=  Get clients
+    Dictionary should contain key  ${clients}  ${NONE}
+    Close client
+    Dictionary should not contain key  ${clients}  ${NONE}
+
+Log WSDL dump
+    Create client  ${CALCULATOR WSDL}
+    Log WSDL dump
+    Close client
+
+Create, switch and close clients
+    Create client  ${CALCULATOR WSDL}  first
+    Create client  ${CALCULATOR WSDL}  alias=second
+
+    ${clients}=  Get clients
+
+    Dictionary should contain key  ${clients}  first
+    Dictionary should contain key  ${clients}  second
+
+    ${active alias}=  Get alias
+    Should be equal as strings  ${active alias}  second
+
+    Switch client  first
+    ${active alias}=  Get alias
+    Should be equal as strings  ${active alias}  first
+
+    Close all clients
+
+Create clients and close other than active
+    Create client  ${CALCULATOR WSDL}  first
+    Create client  ${CALCULATOR WSDL}  alias=second
+    ${clients}=  Get clients
+
+    Close client  first
+    Dictionary should contain key  ${clients}  second
+    Close client
+    Dictionary should not contain key  ${clients}  first
+
+Log opened clients
+    Create client  ${CALCULATOR WSDL}  first
+    Create client  ${CALCULATOR WSDL}  second
+
+    Log opened clients
+    Log opened clients  to_console=${TRUE}
+
+    Close all clients
+
+Namespace trickery
+    Create client  ${CALCULATOR WSDL}
+    Log namespace prefix map
+
+    ${client}=  Get client
+    ${some prefix}=  Set variable  ${client.namespaces.keys()[0]}
+    ${uri}=  Get namespace URI  ${some prefix}
+
+    Should start with  ${uri}  http
+
+    ${some uri}=  Set variable  ${client.namespaces.items()[0][1]}
+    ${prefix}=  Get namespace prefix  ${some uri}
+
+    Close client
+
+Creating a simple message as XML
+    Create client  ${CALCULATOR WSDL}
+
+    ${message}=  Create message  Add
+    ...  a=${10}
+    ...  b=${20}
+
+    Should be true  '${message.__class__.__name__}' == '_Element'
+    Close client
+
+Creating a simple message as string
+    Create client  ${CALCULATOR WSDL}
+
+    ${message}=  Create message  Add  to_string=${TRUE}
+    ...  a=${10}
+    ...  b=${20}
+
+    Should start with  ${message}  <soap-env:Envelope
+    Parse XML  ${message}
+    Close client
+
+Creating an object
+    Create client  ${BLZSERVICE WSDL}
+
+    ${details}=  Create object  ns0:detailsType
+    ...  bezeichnung=Designation
+    ...  bic=Some bank
+    ...  ort=1234
+    ...  plz=n/a
+
+    Should be true  '${details.__class__.__name__}' == 'detailsType'
+    Close client
+
+Creating a more complicated message as XML
+    Create client  ${BLZSERVICE WSDL}
+
+    # It's not actually necessary to create objects in this example,
+    # since it only involves simple types which can be passed to the message
+    # inline. For demonstrative purposes, however, I will create objects
+    # anyways, and pass those.
+
+    ${blz}=  Create object  ns0:getBankType
+    ...  blz=1234
+
+    ${message}=  Create message  getBank
+    ...  blz=${blz}
+
+    Should be true  '${message.__class__.__name__}' == '_Element'
+    Close client
+
+Creating a more complicated message as string
+    Create client  ${BLZSERVICE WSDL}
+
+    # It's not actually necessary to create objects in this example,
+    # since it only involves simple types which can be passed to the message
+    # inline. For demonstrative purposes, however, I will create objects
+    # anyways, and pass those.
+
+    ${blz}=  Create object  ns0:getBankType
+    ...  blz=1234
+
+    ${message}=  Create message  getBank  to_string=${TRUE}
+    ...  blz=${blz}
+
+    Should start with  ${message}  <soap-env:Envelope
+    Close client
